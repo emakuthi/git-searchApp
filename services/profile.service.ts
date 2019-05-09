d8b70abc07509ed48a0704bf3bb55532e64d5912
emakuthi <emakuthi@gmail.com>
2019-05-09 21:43:01 +0300

    create an object in the constructor

--- services/profile.service.ts
+++ services/profile.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable, Subscriber } from 'rxjs';
+import { tap, map, filter } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
-
-  constructor() { }
+private api_key:string;
+private api_url:""
+  constructor(private http:HttpClient) { }
 }
